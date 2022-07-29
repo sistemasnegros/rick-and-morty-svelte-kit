45da79982430379810150b16dc935a102216458b
Alejandro Franco <alejandro.franco@draketech.ca>
2022-07-29 10:52:19 -0500

    fix definitive ssr

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
@@ -11,28 +11,25 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// adapter: adapter(),
+		adapter: adapter(),
 		vite: {
 			optimizeDeps: {
 				entries: []
 			}
-		},
-		adapter: adapter({
-			// default options are shown
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null
-			// precompress: false
-		}),
-		// trailingSlash: 'always',
-		prerender: {
-			default: true
-		},
-		paths: {
-			// YOUR github repository name
-			base: !dev ? '' : '/rick-and-morty-svelte-kit'
-			// assets: dev ? '' : '/rick-and-morty-svelte-kit'
 		}
+		// adapter: adapter({
+		// 	pages: 'docs',
+		// 	assets: 'docs',
+		// 	fallback: null
+		// 	// precompress: false
+		// }),
+		// // trailingSlash: 'always',
+		// prerender: {
+		// 	default: true
+		// },
+		// paths: {
+		// 	base: !dev ? '' : '/rick-and-morty-svelte-kit'
+		// }
 	}
 };
 
